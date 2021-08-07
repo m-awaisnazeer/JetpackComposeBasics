3451348550508afaa1d3d8620bca1eef9ab88480
Awais <m.awaisnaxeer@gmail.com>
2021-08-07 15:34:46 +0500

    Image CardView

--- app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
@@ -4,27 +4,45 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Widget()
+            val painter = painterResource(id = R.drawable.kermit_the_frog)
+            val description = "Kermit in the snow"
+            val title = "title: kermit in the snow"
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(painter, description, title)
+            }
         }
     }
 
 
-    @Preview
     @Composable
     fun Widget() {
         Column(
@@ -48,5 +66,49 @@ class MainActivity : ComponentActivity() {
             Text("World")
         }
     }
+
+
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+        modifier: Modifier = Modifier
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth(), shape = RoundedCornerShape(15.dp),
+            elevation = 15.dp
+        ) {
+            Box(
+                modifier = Modifier.height(200.dp)
+            ) {
+                Image(
+                    painter = painter, contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black,
+                            ),startY = 300f
+                        ))
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp), contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+
+            }
+        }
+
+    }
 }
+
 
