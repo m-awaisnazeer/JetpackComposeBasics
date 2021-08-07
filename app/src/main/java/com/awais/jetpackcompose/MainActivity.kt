170cb8a81446ffd9bafa4088553218689f242a7c
Awais <m.awaisnaxeer@gmail.com>
2021-08-07 15:11:21 +0500

    Compose Modifiers in details

--- app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
@@ -1,74 +1,52 @@
 package com.awais.jetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.awais.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
-    //val name =
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColumnView()
+            Widget()
+        }
+    }
+
+
+    @Preview
+    @Composable
+    fun Widget() {
+        Column(
+            modifier = Modifier
+                .background(Color.Green)
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+                .border(5.dp, Color.Magenta)
+                .padding(5.dp)
+                .border(5.dp, Color.Blue)
+                .padding(5.dp)
+                .border(5.dp, Color.Red)
+                .padding(10.dp)
+        ) {
+            Text(
+                "Hello", modifier = Modifier.clickable {
+                    Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
+                }
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            Text("World")
         }
     }
 }
 
-
-
-@Composable
-fun RowView() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(0.7F)
-            .background(Color.Green),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text("Hello")
-        Text("Awais")
-        Text("Hello")
-
-    }
-}
-
-@Composable
-fun ColumnView() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text("Hello")
-        Text("Awais")
-        Text("Hello")
-
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun ColumnViewPreView() {
-    ColumnView()
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun RowViewPreView() {
-    RowView()
-}
