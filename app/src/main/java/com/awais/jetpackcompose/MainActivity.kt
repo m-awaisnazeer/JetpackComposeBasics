8279065fab04a8b56d665984474f5c9a8f1beef9
Awais <m.awaisnaxeer@gmail.com>
2021-08-07 16:56:58 +0500

    Counter increment with state

--- app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
@@ -3,66 +3,74 @@ package com.awais.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.TextStyle
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(Modifier.fillMaxSize()) {
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-
-                ColorBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize()
-                ) {
-                    color.value = it
-                }
-                Box(
-                    modifier = Modifier
-                        .background(color.value)
-                        .weight(1f)
-                        .fillMaxSize()
-
-                ) {
-
-                }
-            }
+            TextViewWithIncrementButton()
         }
     }
 }
 
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    updateColor: (Color) -> Unit
-) {
-    Box(modifier = modifier
-        .background(Color.Red)
-        .clickable {
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
 
-        }) {
+@Preview
+@Composable
+fun TextViewWithIncrementButton() {
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val valueInt = remember {
+            mutableStateOf(0)
+        }
+
+        val painter = painterResource(id = R.drawable.ic_baseline_add_circle_outline_24)
+        val description = "Kermit in the snow"
+        val title = "title: kermit in the snow"
+
+        Text(
+            text = valueInt.value.toString(),
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 64.sp
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Image(
+            painter = painter, contentDescription = description,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clickable {
+                    valueInt.value++
+
+                }
+                .width(48.dp)
+                .height(48.dp)
+        )
+
 
     }
 }
+
+
