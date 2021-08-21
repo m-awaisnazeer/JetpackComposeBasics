bb22b034cf71fd08b896acba9dd8716118ee8383
Awais <m.awaisnaxeer@gmail.com>
2021-08-21 14:07:15 +0500

    Simple Animations with tween,spring,keyframes and infiniteTransition

--- app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/awais/jetpackcompose/MainActivity.kt
@@ -3,11 +3,19 @@ package com.awais.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.awais.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +23,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                var sizeState by remember {
+                    mutableStateOf(200.dp)
+                }
+                val size by animateDpAsState(
+                    targetValue =
+                    sizeState,
+                    /*
+                        animationSpec = tween(
+                        durationMillis = 3000,
+                        delayMillis = 300,
+                        easing = FastOutSlowInEasing
+                    )
+                    spring(
+                        Spring.DampingRatioHighBouncy,
+                    )
+                    keyframes {
+                        durationMillis = 5000
+                        sizeState at 0 with LinearEasing
+                        sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                        sizeState * 2f at 5000
+                    }
+                     */
+
+                )
+
+                val infiniteTransition = rememberInfiniteTransition()
+                val color by infiniteTransition.animateColor(
+                    initialValue = Color.Red,
+                    targetValue = Color.Green,
+                    animationSpec = infiniteRepeatable(
+                        tween(durationMillis = 2000),
+                        repeatMode = RepeatMode.Reverse
+                    )
+                )
+                Box(
+                    modifier = Modifier
+                        .size(size)
+                        .background(color),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(onClick = { sizeState += 50.dp }) {
+                        Text(text = "Increase Size")
+                    }
                 }
             }
         }
@@ -26,7 +74,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
 }
 
 @Preview(showBackground = true)
